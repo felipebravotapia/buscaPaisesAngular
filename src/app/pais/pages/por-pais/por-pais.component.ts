00c43d2393ba8ce2bafc1559d4e0575c11cb1015
Felipe30 <fbravo@pentafinanciero.cl>
2021-02-03 19:01:49 -0300

    busqueda con sugerencias (onDebounce)

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,18 +5,20 @@ import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [],
+  styles: [`li{ cursor:pointer}`],
 })
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostarSugerencias: boolean = false;
 
-  constructor(private paisService: PaisService) {}
+  constructor(private paisService: PaisService) { }
 
   buscar(termino: string) {
+    this.mostarSugerencias = false;
     this.hayError = false;
-
     this.termino = termino;
 
     this.paisService.buscarPais(this.termino).subscribe(
@@ -33,7 +35,18 @@ export class PorPaisComponent {
       }
     );
   }
-  sugerencias( termino: string){
+  sugerencias(termino: string) {
     this.hayError = false;
+    this.termino = termino;
+    this.mostarSugerencias = true;
+    this.paisService.buscarPais(termino)
+      .subscribe(paises => this.paisesSugeridos = paises.splice(0, 5)
+        , (err) => this.paisesSugeridos = [])
   }
+
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
+
+  }
+
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,19 +10,28 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
   private apiUrl = 'https://restcountries.eu/rest/v2';
 
-  constructor(private http: HttpClient) {}
+  get httpParamas() {
+    return new HttpParams().set('fields', 'name;capital;alpha2Code;flag;population');
+  }
+
+  constructor(private http: HttpClient) { }
 
   buscarPais(termino: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParamas });
   }
 
   buscarCapital(termino: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParamas });
   }
   getPaisPorAlpha(id: string): Observable<Country> {
     const url = `${this.apiUrl}/alpha/${id}`;
     return this.http.get<Country>(url);
+  }
+  buscarRegion(region: string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/region/${region}`;
+    return this.http.get<Country[]>(url, { params: this.httpParamas }).pipe(tap(console.log))
   }
 }
